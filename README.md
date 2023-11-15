2862e5743c21fc02e94362ef35dc4ee111fc010e
Sergio Broccardi <sbroccardi@gmail.com>
2023-11-15 13:24:22 -0300

    version 0.2.2

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# ari-ib-calificaciones-api
+# Apoyo y Regímenes Informativos - IB.Calificaciones API
+
+## Comenzando 🚀
+
+_Estas instrucciones permiten obtener una copia del proyecto en funcionamiento en tu máquina local para propósitos de desarrollo y pruebas._
+
+### Pre-requisitos 📋
+
+* [.NET 6+](https://dotnet.microsoft.com/)
+
+### Creación desde cero 🔧
+
+_Una serie de ejemplos paso a paso que debes ejecutar para tener un entorno de desarrollo ejecutandose_
+
+1. Configuración
+
+_BNA.IB.Calificaciones.API.Web/appsettings.Development.json_
+
+2. Build
+
+   ```bash
+   dotnet build
+   ```
+
+### Base de datos 🔧
+
+_Instrucciones para conectarse a la base de datos, crear y ejecutar migraciones de EF_
+
+1. Iniciar SQL Server mediante Docker (opcional)
+
+   ```bash
+   docker run --cap-add SYS_PTRACE -e 'ACCEPT_EULA=1' -e 'MSSQL_SA_PASSWORD=bnapassword' -p 1433:1433 --name azuresqledge -d mcr.microsoft.com/azure-sql-edge
+   ```
+
+2. Agregar nueva migración (desde BNA.IB.Calificaciones.API.Infrastructure.SQLServer)
+
+   ```bash
+   dotnet ef migrations add InitialCreate -s ../BNA.IB.Calificaciones.API.Web
+   ```
+
+3. Ejecutar migración en base de datos (desde BNA.IB.Calificaciones.API.Infrastructure.SQLServer)
+
+   ```bash
+   dotnet ef database update -s ../BNA.IB.Calificaciones.API.Web/ --configuration Development
+   ```
+
+## Construido con 🛠️
+
+_herramientas utilizadas para crear el backend_
+
+- [.NET 6+](https://dotnet.microsoft.com/)
+
+\+ other smaller dependencies
+
+## Versionado 📌
+
+Usamos [SemVer](http://semver.org/) para el versionado. Para todas las versiones disponibles, mira los [tags en este repositorio](https://github.com/Banco-Nacion/ari-ib-calificaciones-api/tags).
+
+## Documentación
+- 
---
+++ src/BNA.IB.Calificaciones.API.Infrastructure.SQLServer/Migrations/20231115155453_InitialCreate.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BNA.IB.Calificaciones.API.Infrastructure.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BCRACalificaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Calificacion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BCRACalificaciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calificadoras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Clave = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FechaAltaBCRA = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBajaBCRA = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calificadoras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalificadoraPeriodos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CalificadoraId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalificadoraPeriodos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CalificadoraPeriodos_Calificadoras_CalificadoraId",
+                        column: x => x.CalificadoraId,
+                        principalTable: "Calificadoras",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TituloPersonaCalificadas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CalificadoraId = table.Column<int>(type: "int", nullable: false),
+                    BcraCalificacionId = table.Column<int>(type: "int", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaAlta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaBaja = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TituloPersonaCalificadas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TituloPersonaCalificadas_BCRACalificaciones_BcraCalificacionId",
+                        column: x => x.BcraCalificacionId,
+                        principalTable: "BCRACalificaciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TituloPersonaCalificadas_Calificadoras_CalificadoraId",
+                        column: x => x.CalificadoraId,
+                        principalTable: "Calificadoras",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalificadoraPeriodoEquivalencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CalificadoraPeriodoId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalificadoraPeriodoEquivalencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CalificadoraPeriodoEquivalencias_CalificadoraPeriodos_CalificadoraPeriodoId",
+                        column: x => x.CalificadoraPeriodoId,
+                        principalTable: "CalificadoraPeriodos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equivalencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BcraCalificacionId = table.Column<int>(type: "int", nullable: false),
+                    CalificacionCalificadora = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CalificadoraPeriodoEquivalenciaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equivalencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equivalencias_BCRACalificaciones_BcraCalificacionId",
+                        column: x => x.BcraCalificacionId,
+                        principalTable: "BCRACalificaciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Equivalencias_CalificadoraPeriodoEquivalencias_CalificadoraPeriodoEquivalenciaId",
+                        column: x => x.CalificadoraPeriodoEquivalenciaId,
+                        principalTable: "CalificadoraPeriodoEquivalencias",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalificadoraPeriodoEquivalencias_CalificadoraPeriodoId",
+                table: "CalificadoraPeriodoEquivalencias",
+                column: "CalificadoraPeriodoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CalificadoraPeriodos_CalificadoraId",
+                table: "CalificadoraPeriodos",
+                column: "CalificadoraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equivalencias_BcraCalificacionId",
+                table: "Equivalencias",
+                column: "BcraCalificacionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equivalencias_CalificadoraPeriodoEquivalenciaId",
+                table: "Equivalencias",
+                column: "CalificadoraPeriodoEquivalenciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TituloPersonaCalificadas_BcraCalificacionId",
+                table: "TituloPersonaCalificadas",
+                column: "BcraCalificacionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TituloPersonaCalificadas_CalificadoraId",
+                table: "TituloPersonaCalificadas",
+                column: "CalificadoraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Equivalencias");
+
+            migrationBuilder.DropTable(
+                name: "TituloPersonaCalificadas");
+
+            migrationBuilder.DropTable(
+                name: "CalificadoraPeriodoEquivalencias");
+
+            migrationBuilder.DropTable(
+                name: "BCRACalificaciones");
+
+            migrationBuilder.DropTable(
+                name: "CalificadoraPeriodos");
+
+            migrationBuilder.DropTable(
+                name: "Calificadoras");
+        }
+    }
+}
